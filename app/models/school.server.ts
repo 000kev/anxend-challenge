82194bb60cc1ed0b4b9eb91615606632ba5560ae
Kevin <kumasambakyg@gmail.com>
2024-02-29 13:00:40 +0200

    Succesfully posting data from form to the database

--- app/models/school.server.ts
+++ app/models/school.server.ts
@@ -1,7 +1,9 @@
 import { client } from "#app/utils/db.server"
 import e from "../../dbschema/edgeql-js/index"
 
-const School = { name: "Anxend School" } // Just an example this won't actually run unless it was called.
+
+
+// const School = { name: "Anxend School" } // Just an example this won't actually run unless it was called.
 
 // const newSchool = await e.insert(e.School, School).run(client)
 
--- app/routes/project_one.tsx
+++ app/routes/project_one.tsx
@@ -6,7 +6,6 @@ import e from "../../dbschema/edgeql-js";
 import { redirect } from "@remix-run/node";
 
 
-
 // export const loader = async () => {
 //     // const result = await client.query(`select 2 + 2;`)
 //     // let message = "The result is " + result + "\nThe db query was succesful.";
@@ -17,15 +16,53 @@ import { redirect } from "@remix-run/node";
 //     const result = await query.run(client);
 //     return "Successful add";
 // }
-export const action = async ({request}: ActionFunctionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
     const formData = await request.formData();
-    const name = formData.get("name");
-    const description = formData.get("description");
+
+    const name = formData.get("name").toString();
+    const isSenior = formData.get("juniorSenior").toString() === "senior";
+    const country = formData.get("country").toString();
+    const address_code = formData.get("addressCode").toString();
+    const town_city = formData.get("townCity").toString();
+    const name_number = formData.get("nameNumber").toString();
+    const region = formData.get("region").toString();
+    const street1 = formData.get("street1").toString();
+    const street2 = formData.get("street2").toString();
+
     // e.insert(e.School, {
     //     name: "St. Martins"
     // }).run(client);
 
-    return {name: name, description: description};
+    let entry = {
+        name: name,
+        juniorSchool: false,
+        seniorSchool: true,
+        country: country,
+        addressCode: address_code,
+        townOrCity: town_city,
+        nameOrNumber: name_number,
+        region: region,
+        street: street1,
+        street2: street2
+    }
+
+    const entryQuery = e.insert(e.Address, {
+        addressCode: address_code,
+        country: country,
+        nameOrNumber: name_number,
+        townOrCity: town_city,
+        region: region,
+        street: street1,
+        street2: street2,
+        organisation: e.insert(e.School, {
+            name: name,
+            seniorSchool: isSenior,
+            juniorSchool: !isSenior
+        })
+    });
+    entryQuery.run(client);
+
+    return entry;
 }
 
 export default function ProjectOne() {
@@ -34,23 +71,72 @@ export default function ProjectOne() {
 
     return (
         <Form className="flex-col" method="post" action="/project_one">
-        <p>
-          <label>
-            School:
-            <br />
-            <input name="name" type="text"/>
-          </label>
-        </p>
-        <p>
-          <label>
-            Address:
-            <br />
-            <textarea name="description" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Create</button>
-        </p>
-      </Form>
+            <span>
+                <label>
+                    School:
+                    <br />
+                    <input name="name" type="text" />
+                </label>
+                <select name="juniorSenior">
+                    <option value="senior">Senior School</option>
+                    <option value = "junior">Junior School</option>
+                </select>
+            </span>
+            <div>
+                <select name="country">
+                    <option>South Africa</option>
+                    <option>England</option>
+                    <option>Germany</option>
+                    <option>France</option>
+                    <option>Brazil</option>
+                    <option>USA</option>
+                </select>
+            </div>
+            <div>
+                <label>
+                    Address Code:
+                    <br />
+                    <input name="addressCode" type="text" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Town/City:
+                    <br />
+                    <input name="townCity" type="text" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Region:
+                    <br />
+                    <input name="region" type="text" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Street1
+                    <br />
+                    <input name="street1" type="text" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Street2:
+                    <br />
+                    <input name="street2" type="text" />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Street Name/Number
+                    <br />
+                    <input name="nameNumber" type="text" />
+                </label>
+            </div>
+            <div>
+                <button type="submit">Create</button>
+            </div>
+        </Form>
     );
 }
